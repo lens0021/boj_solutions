c468c1fab8bddb7046255b3f4ac7db021b70eb26
lens0021 <lorentz0021@gmail.com>
2019-07-20 16:56:13 +0900

    Complete iterative solution of 2098

--- 2098.py
+++ 2098.py
@@ -61,7 +61,7 @@ class BitSet:
 
         return lst
 
-    def not_all(self):
+    def everything_is_false(self):  # 이거 영어로 뭐라 적어요
         return self._int == 0
 
     def copy(self):
@@ -69,7 +69,7 @@ class BitSet:
 
 
 NUMBER_OF_VERTICES = int(sys.stdin.readline())
-distances = [
+DISTANCES = [
     [float('inf') if str == '0' else int(str)
      for str in sys.stdin.readline().split()]
     for _ in range(NUMBER_OF_VERTICES)
@@ -77,27 +77,89 @@ distances = [
 
 
 @lru_cache(None)
-def min_weight_using_vertices(destination, usable_vertices: BitSet, rec_level=0):
-    if usable_vertices.not_all():
-        return distances[0][destination]
+def min_weight_using_vertices(destination, usable_vertices: BitSet):
+    if usable_vertices.everything_is_false():
+        return DISTANCES[0][destination]
 
-    sub_distances = {}
+    sub_distances = []
     for i in usable_vertices.indices_of_trues():
-        if distances[i][destination] == 0:
+        if DISTANCES[i][destination] == 0:
             continue
 
         new_usable_vertices = usable_vertices.copy()
         new_usable_vertices.set(i, False)
-        sub_distances[i] = min_weight_using_vertices(
-            i, new_usable_vertices,
-            rec_level+1
-        ) + distances[i][destination]
+        sub_distances.append(
+            min_weight_using_vertices(
+                i, new_usable_vertices) + DISTANCES[i][destination]
+        )
 
-    return min(sub_distances.values())
+    return min(sub_distances)
 
 
-print(distances)
+def digit_sum(num: int, base=2):
+    sum = 0
+    while num > 0:
+        sum += num % base
+        num //= base
+    return sum
+
+
+def iteratively_find_min_weight():
+    memo = [
+        [float('inf') for _ in range(NUMBER_OF_VERTICES)]
+        for _ in range(2**(NUMBER_OF_VERTICES-1))
+    ]
+
+    sequence = list(range(1, 2**(NUMBER_OF_VERTICES-1)))
+    sequence.sort(key=digit_sum)
+
+    for vertex in range(NUMBER_OF_VERTICES):
+        memo[0][vertex] = DISTANCES[0][vertex]
+
+    for bitset in sequence:
+        vertex = 0
+        visited, unvisited = [], []
+        for i in range(1, NUMBER_OF_VERTICES):
+            if (bitset >> (i-1)) & 1:
+                visited.append(i)
+            else:
+                unvisited.append(i)
+
+        if bitset == 2**(NUMBER_OF_VERTICES-1)-1:
+            unvisited.append(0)
+
+        # print('bitset:', format(bitset, '03b'), end=' ')
+        # print('( visited:', visited, 'unvisited:', unvisited, ')')
+
+        for destination in unvisited:
+            # print(f'To go {destination}', end=' ')
+            min_distance = float('inf')
+            for waypoint in visited:
+                waypoint_bitset = bitset & ~(1 << (waypoint-1))
+                # print(
+                #     'using',
+                #     waypoint, end='')
+                # print(
+                #     f' (memo[{format(waypoint_bitset,"03b")}][{waypoint}]({memo[waypoint_bitset][waypoint]})', end='+')
+                # print(
+                #     f'distance({DISTANCES[waypoint][destination]})', end='=')
+                # print(
+                #     f'{memo[waypoint_bitset][waypoint]+DISTANCES[waypoint][destination]})')
+                min_distance = min(
+                    min_distance,
+                    memo[waypoint_bitset][waypoint] +
+                    DISTANCES[waypoint][destination]
+                )
+            memo[bitset][destination] = min_distance
+
+            # print(
+            #     f'Set memo[{format(bitset, "03b")}][{destination}] to {memo[bitset][destination]}')
+    return memo[-1][0]
+
+
+# print(DISTANCES)
 all_but_zero = BitSet.new_from_number(2 ** NUMBER_OF_VERTICES - 2)
 
-# print(brute_force(distances))
-print(min_weight_using_vertices(0, all_but_zero))
+# print(brute_force(DISTANCES))
+# print(min_weight_using_vertices(0, all_but_zero))
+print(iteratively_find_min_weight())
