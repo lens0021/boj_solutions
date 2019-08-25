366444a926f31d8dfe314a4f9304cb6b6d717e1f
lens0021 <lorentz0021@gmail.com>
2019-08-25 18:20:58 +0900

    Update 11650.py

--- 11650.py
+++ 11650.py
@@ -7,11 +7,9 @@ stdout.write(
         f'{v // 1000000 - 100000} {v % 1000000 - 100000}'
         for v
         in sorted(
-            (v[0]+100000)*1000000 + v[1]+100000 for v in
-            [
-                tuple(map(int, line.split()))
-                for line in stdin.read().splitlines()[1:]
-            ]
+            (int(line.split()[0])+100000) * 1000000
+            + int(line.split()[1])+100000
+            for line in stdin.read().splitlines()[1:]
         )
     ) + '\n'
 )
