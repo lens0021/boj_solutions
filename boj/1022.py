a37a7b964ad4d21813eb1526752934cdb873d77c
lens0021 <lorentz0021@gmail.com>
2019-09-22 19:16:30 +0900

    Fix bug

--- boj/1022.py
+++ boj/1022.py
@@ -1,45 +1,47 @@
 # https://www.acmicpc.net/problem/1022
 
 DIRECTION = (
-    (1, 0),
-    (0, -1),
+    (0, 1),
     (-1, 0),
-    (0, 1)
+    (0, -1),
+    (1, 0),
 )
 
 
-MIN_X, MIN_Y, MAX_X, MAX_Y = map(int, input().split(' '))
+r1, c1, r2, c2 = map(int, input().split(' '))
 _map = {}
 
 num = 1
-x, y = 0, 0
+r, c = 0, 0
 direction = 0
 cnt = 0
-while x < max(-MIN_X, MAX_X)+1 or y < max(-MIN_Y, MAX_Y)+1:
+_max = max(abs(r1), abs(c1), abs(r2), abs(c2))+1
+while r <= _max and c <= _max:
     for move in range(cnt//2 + 1):
-        if MIN_X-2 <= x <= MAX_X+2 and MIN_Y-2 <= y <= MAX_Y+2:
-            _map[x*10000+y] = num
+        if r1 <= r <= r2 and c1 <= c <= c2:
+            _map[r*10000+c] = num
         num += 1
 
-        x += DIRECTION[direction][0]
-        y += DIRECTION[direction][1]
+        r += DIRECTION[direction][0]
+        c += DIRECTION[direction][1]
 
     direction = (direction + 1) % 4
     cnt += 1
 
 w = 1
-for v in range(MIN_X, MAX_X+1):
-    for u in range(MIN_Y, MAX_Y+1):
-        if u*10000+v in _map:
-            w = max(w, len(str(_map[u*10000+v])))
-
+for r in range(r1, r2+1):
+    for c in range(c1, c2+1):
+        if r*10000+c in _map:
+            w = max(w, len(str(_map[r*10000+c])))
 
 print('\n'.join(
     ' '.join([
-        (str(_map[u*10000+v]).rjust(w)
-         if u * 10000+v in _map
-         else ' ' * w)
-        for u in range(MIN_Y, MAX_Y+1)
+        (
+            str(_map[r*10000+c]).rjust(w)
+            if r*10000 + c in _map
+            else ' ' * w
+        )
+        for c in range(c1, c2+1)
     ])
-    for v in range(MIN_X, MAX_X+1)
+    for r in range(r1, r2+1)
 ))
