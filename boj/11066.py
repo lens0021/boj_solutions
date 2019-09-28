95f1ce387c561379304adce3e7db9a01eda4b638
lens0021 <lorentz0021@gmail.com>
2019-09-28 21:29:49 +0900

    WA

--- boj/11066.py
+++ boj/11066.py
@@ -1,30 +1,32 @@
 # https://www.acmicpc.net/problem/11066
 
 from sys import stdin
-from math import log2, floor
+from functools import lru_cache
 
 
-def min_cost(lst):
-    l = len(lst)
-    if l == 0:
-        return 0
-    if l == 1:
-        return lst[0]
-    p = floor(log2(l))
-    if l == 2**p:
-        rt = sum(lst)*p
+@lru_cache(None)
+def solution(case, left, right):
+    ''' [left, right) '''
+    l = right - left
+    if l <= 1:
+        rt = 0
     else:
-        rt = sum(lst[:2**p])*p + min_cost(lst[2**p:])
-
-    print(lst, '=>', rt, '|', p)
+        rt = min(
+            solution(case, left, partition)
+            + solution(case, partition, right)
+            + sum(numbers[case][left:right])
+            for partition in range(left+1, right)
+        )
     return rt
 
 
 lines = stdin.readlines()
-for case in range(int(lines[0][0])):
-    n = int(lines[case*2+1])
-    sizes = list(map(int, lines[case*2+2].split(' ')))
-    print(sizes)
-    print(min_cost(sizes))
+number_of_testcases = int(lines[0][0])
+numbers = [None for _ in range(number_of_testcases)]
+output = ''
+for case in range(number_of_testcases):
+    length_of_numbers = int(lines[case*2+1])
+    numbers[case] = list(map(int, lines[case*2+2].split(' ')))
+    output += str(solution(case, 0, length_of_numbers))+'\n'
 
-# print(min_cost([1, 3, 3, 4, 4, 5, 5, 5]))
+print(output, end='')
